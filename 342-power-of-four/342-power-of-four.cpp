ffb18c4cad866df717141ab16a6bc52e420240b9
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-08-24 14:38:15 +0530

    Time: 7 ms (13.19%), Space: 5.8 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==INT_MIN)
+        if(n<1)
             return false;
         if((n & (n-1))==0 && (n%3 == 1)){
             return true;
