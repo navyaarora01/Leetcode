e51dbb861ab64e1aef72eea13469f7dafed77277
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-08-30 22:59:50 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -7,14 +7,14 @@ public:
                 swap(matrix[i][j] , matrix[j][i]);
             }
         }
-        for(int i = 0 ;i<n;i++){
-            for(int j = 0;j<n/2;j++){
-                swap(matrix[i][j] , matrix[i][n-j-1]);
-            }
+//         for(int i = 0 ;i<n;i++){
+//             for(int j = 0;j<n/2;j++){
+//                 swap(matrix[i][j] , matrix[i][n-j-1]);
+//             }
             
+//         }
+        for(int  i = 0;i<n;i++){
+            reverse(matrix[i].begin() , matrix[i].end());
         }
-        // for(int  i = 0;i<n;i++){
-        //     reverse(matrix[i].begin() , matrix[i].end());
-        // }
     }
 };
