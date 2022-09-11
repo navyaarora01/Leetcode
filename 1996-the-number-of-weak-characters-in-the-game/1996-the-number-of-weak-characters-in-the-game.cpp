fc60c4d53a0c1325a762cae56da60db8b2c82638
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-09-11 11:52:40 +0530

    Time: 782 ms (88.72%), Space: 125.2 MB (63.53%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,27 +1,22 @@
-  class Solution
-{
+class Solution {
 public:
-     //handling the edge case while sorting
-     static bool comp(vector<int> &a, vector<int> &b)
-     {
-          if (a[0] == b[0])
-          {
-               return a[1] > b[1];
-          }
-          return a[0] < b[0];
-     }
-     int numberOfWeakCharacters(vector<vector<int>> &properties)
-     {
-          sort(properties.begin(), properties.end(), comp); //sorting the array
-          int mtn = INT_MIN;                                //max till now while moving from right
-          int ans = 0;
-
-          for (int i = properties.size() - 1; i >= 0; i--)
-          {
-               if (properties[i][1] < mtn) // if the second parameter is also less increase the ans
-                    ans++;
-               mtn = max(mtn, properties[i][1]);
-          }
-          return ans;
-     }
+    int numberOfWeakCharacters(vector<vector<int>>& prop) {
+        int n = prop.size();
+        sort(prop.begin() , prop.end() , [&](vector<int> &a , vector<int> &b){
+            if(a[0]==b[0]){
+                return a[1]>b[1];
+            }
+            return a[0]<b[0];
+        });
+        
+        int maxDef = INT_MIN;
+        int ans = 0;
+        for(int i = n-1;i>=0;i--){
+            if(prop[i][1]<maxDef){
+                ans++;
+            }
+            maxDef = max(maxDef , prop[i][1]);
+        }
+        return ans;
+    }
 };
