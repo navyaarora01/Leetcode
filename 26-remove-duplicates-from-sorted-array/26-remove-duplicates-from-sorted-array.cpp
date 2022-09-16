a5a4e59447d93b619e2264458ea692dfb06fafb7
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-09-16 21:37:10 +0530

    Time: 30 ms (21.46%), Space: 19.2 MB (6.16%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& arr) {
-        int n = arr.size();
-         set < int > set;
-  for (int i = 0; i < n; i++) {
-    set.insert(arr[i]);
-  }
-  int k = set.size();
-  int j = 0;
-  for (int x: set) {
-    arr[j++] = x;
-  }
-  return k;
+    int removeDuplicates(vector<int>&nums) {
+  //       int n = arr.size();
+  //        set < int > set;
+  // for (int i = 0; i < n; i++) {
+  //   set.insert(arr[i]);
+  // }
+  // int k = set.size();
+  // int j = 0;
+  // for (int x: set) {
+  //   arr[j++] = x;
+  // }
+  // return k;
         
         
         
@@ -22,22 +22,19 @@ public:
         
         
         
-        
-//         set<int> s;
-//         int count=0;
-//         for(auto x: nums){
-//             s.insert(x);
-//             count++;
+        int n = nums.size();
+        set<int> s;
+        for(auto x: nums){
+            s.insert(x);
+            // count++;
             
-//         }
-//         nums.clear();
-//         for(auto x:s){
-//             // cout<<x<<endl;
-//             nums.push_back(x);
-//         }
-//         // cout<<nums[0]<<endl;
-//         cout<<nums[1]<<endl;
-//         cout<<nums[2]<<endl;
-//         return count;
+        }
+        nums.clear();
+        int i=0;
+        for(int x:s){
+            // cout<<x<<endl;
+            nums.push_back(x);
+        }
+        return s.size();
     }
 };
