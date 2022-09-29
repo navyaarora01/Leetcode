a687aba0d3652fdd485043779f9f55a77dcad8c9
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-09-29 15:10:04 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -8,7 +8,7 @@ public:
         if(n<=2){
             return dp[n];
         }
-        for(int i = 3;i<n;i++){
+        for(int i = 3;i<=n;i++){
             dp[i] = dp[i-1]+dp[i-2]; 
         }
         return dp[n-1]+dp[n-2];
