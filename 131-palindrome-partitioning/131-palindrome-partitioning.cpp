e963690cf5b04a7fd9736a0448b026ce29b1ff10
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-09-30 09:43:16 +0530

    Time: 420 ms (14.42%), Space: 84.1 MB (28.26%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:    
+    vector<vector<string>> ans;
+    vector<vector<string>> partition(string s) {
+        vector<string> path;
+        func(0 , s , path);
+        return ans;
+    }
+    
+    void func(int index , string s , vector<string>& path){
+        if(index ==s.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i = index ; i<s.size() ; i++){
+            if(isPalindrome(s , index , i)){
+                path.push_back(s.substr(index , i-index+1));
+                func(i+1 , s , path);
+                path.pop_back();
+            }
+        }
+    }
+    
+    bool isPalindrome(string s , int start , int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return false;
+            }
+            
+        }        return true;
+    }
+    
+};
