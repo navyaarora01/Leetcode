0c25726dbda27d47d073c55aa139297d67605173
Navya Arora <navya3623.be20@chitkara.edu.in>
2022-10-08 21:53:15 +0530

    Time: 461 ms (52.31%), Space: 16.3 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -3,20 +3,20 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int ans = 100000, temp;
+        int temp = INT_MAX, ans;
         for(int i = 0;i < n; i++){
             int left = i + 1, right = n - 1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if(ans > abs(target - sum)){
-                    ans = abs(target - sum);
-                    temp = sum;
+                if(temp > abs(target - sum)){
+                    temp = abs(target - sum);
+                    ans = sum;
                 }
                 if(sum < target)    left++;
                 else if(sum == target)   return sum;
                 else    right--;
             }
         }
-        return temp;
+        return ans;
     }
 };
